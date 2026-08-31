8978e4f115a2550f8f1a28021dc220fc11d4d944
David Czerski <bassistcz@hotmail.com>
2026-08-31 19:50:40 +0100

    added uuid, fixed some paths, update to DB

--- tools/clear_database.py
+++ tools/clear_database.py
@@ -1,6 +1,7 @@
 import sqlite3
+from pathlib import Path
 
-DB_PATH = "../database/readings.db"
+DB_PATH = Path(__file__).resolve().parent.parent / "database" / "readings.db"
 
 con = sqlite3.connect(DB_PATH)
 
